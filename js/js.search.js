037329b92279dc4377e92fc414442f5743e5ad6e
현중 <hjhjon12@gmail.com>
2025-05-07 14:26:09 +0900

    10주차 수업완료

--- js/js.search.js
+++ js/js.search.js
@@ -1,4 +1,5 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
+
  function search_message(){
  alert("검색을 수행합니다!");
  }
@@ -7,10 +8,8 @@ document.getElementById("search_btn").addEventListener('click', search_message);
  //const c = '검색을 수행합니다';
  //alert(c);
  // };
+ 
  function googleSearch() {
-  
-
-   
     const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
     const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
      // 새 창에서 구글 검색을 수행
